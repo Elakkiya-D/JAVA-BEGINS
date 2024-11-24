75d47540633f0e71b69d813a8841154826a7b6f7
Elakkiya <delakkiya2@gmail.com>
2024-11-24 20:55:26 +0530

    logical operator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,23 @@
 package org.example;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//Relational  operator
+//logical  operator
         int a=54;
         int b=20;
-        System.out.println(a<b);
-        System.out.println(a>b);
-        System.out.println(a>=b);
-
-        System.out.println(a<=b);
-        System.out.println(a!=b);
-        System.out.println(a==b);
+        System.out.println(a<b && a>b);
+        System.out.println(a>b || a<b);
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter Q or q to quit the game");
+        String response = scanner.nextLine();
+        if(response.equals("q") || response.equals("Q")) {
+            System.out.println("you quit the game");
+        }
+        else {
+            System.out.println("your Still playing");
+        }
 
 
 
