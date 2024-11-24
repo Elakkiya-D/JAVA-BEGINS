720159b533dd4d948ee13444eddbf91a58ee85ce
Elakkiya <delakkiya2@gmail.com>
2024-11-24 14:50:08 +0530

    Implicit and Explicit conversion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,23 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 System.out.println("Sri rama Jayam");
+//Explicit Type Casting
+float a= 5.0F;
+int b=(int)a;
+System.out.println(b);
+double d=99778.667;
+long l=(long)d;
+
+//Implicit Type Casting
+System.out.println(l);
+int c=88;
+float f=c;
+System.out.println(f);
+
+long g= 6735678L;
+double h=g;
+System.out.println(h);
+
 
     }
 }
