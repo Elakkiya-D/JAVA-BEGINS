5e6708fc5cc14f5187cc99cac5c1242afc3de92f
Elakkiya <delakkiya2@gmail.com>
2024-11-24 20:30:21 +0530

    Relational  operator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,16 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-//arithmetic operator
+//Relational  operator
         int a=54;
         int b=20;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-//54=5+4=9
-        System.out.println((a/10)+(a%10));
-        System.out.println(a/b);
-        System.out.println(a%b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+
+        System.out.println(a<=b);
+        System.out.println(a!=b);
+        System.out.println(a==b);
 
 
 
