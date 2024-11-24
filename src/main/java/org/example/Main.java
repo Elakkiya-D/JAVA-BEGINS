658ee0d0a5c8fc66dc50969c55bbce2edc212a01
Elakkiya <delakkiya2@gmail.com>
2024-11-24 20:59:56 +0530

    bitwise  operator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//logical  operator
-        int a=54;
+//lift and right shift operator
+        int a=10;
         int b=20;
-        System.out.println(a<b && a>b);
-        System.out.println(a>b || a<b);
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("enter Q or q to quit the game");
-        String response = scanner.nextLine();
-        if(response.equals("q") || response.equals("Q")) {
-            System.out.println("you quit the game");
-        }
-        else {
-            System.out.println("your Still playing");
-        }
+        System.out.println(a<<b);
+        System.out.println(b>>a);
+        System.out.println(a&b);
+        //here binary values are to be used
+        System.out.println(a|b);
+
+
+
+
 
 
 
