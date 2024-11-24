2efb68c5e2cdddc3e632b79387cb4e14e197c0a9
Elakkiya <delakkiya2@gmail.com>
2024-11-24 20:27:16 +0530

    unary Operator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,15 @@
 package org.example;
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        //Unary operator
-        //unary plus
-        int a=+1;
-        //unary minus
-        int b=-a;
-
-        a++;
-        ++a;
-        b++;
-        ++b;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(a++ + b++);
-         System.out.println(++a + --b);
-        b--;
-        --b;
-        --a;
-        a--;
-        System.out.println(a);
-        System.out.println(b);
-
-
-
-
-
-
-
-
-
+//arithmetic operator
+        int a=54;
+        int b=20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+//54=5+4=9
+        System.out.println((a/10)+(a%10));
+        System.out.println(a/b);
 
 
 
